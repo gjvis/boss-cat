48ed57ee2040aed984878d5a08194704d1e34981
Gareth Visagie <gareth@gjvis.com>
2014-11-06 16:47:42 +0000

    Initial commit

---
+++ cat.js
@@ -0,0 +1,22 @@
+module.exports = class Cat {
+
+  constructor(name='Simon') {
+    this.name = name;
+  }
+
+  meow() {
+    return 'MROW'
+  }
+
+  destroyTheHouse() {
+    window['Object'] = undefined
+  }
+
+  eat() {
+    return 'nom nom nom'
+  }
+
+  sleep(callback) {
+    setTimeout(callback, 1000)
+  }
+}
---
+++ test/test-cat.js
@@ -0,0 +1,41 @@
+require("6to5/register");
+
+var Cat = require('./../'),
+    test = require('tape');
+
+test('sanity check', function(t) {
+  t.equal(typeof Cat, 'function', 'Cat exists');
+  t.end();
+});
+
+test('its default name', function(t) {
+  var cat = new Cat();
+  t.equal(cat.name, 'Simon', 'is Simon');
+  t.end();
+});
+
+test('if I call it bob', function(t) {
+  var cat = new Cat('Bob');
+  t.equal(cat.name, 'Bob', 'then its name is Bob');
+  t.end();
+})
+
+test('its meow', function(t) {
+  var cat = new Cat();
+  t.equal('MROW', cat.meow(), 'is loud');
+  t.end();
+});
+
+test('its eating', function(t) {
+  var cat = new Cat();
+  t.equal('nom nom nom', cat.eat(), 'is nomtastic')
+  t.end();
+});
+
+test('after it sleeps', function(t) {
+  var cat = new Cat();
+  cat.sleep(function() {
+    t.pass('it wakes');
+    t.end();
+  })
+});
